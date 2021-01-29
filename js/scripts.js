e32a37edae7b0d57ef677dddceee75a821552550
Sage Sypert <sage.sypert@gmail.com>
2021-01-29 15:33:47 -0800

    add submit function for order form, push values for veggies into empty array

--- js/scripts.js
+++ js/scripts.js
@@ -44,34 +44,16 @@ Pizza.prototype.calculatePrice = function() {
 
 }
 
-  
-
-// Pizza.prototype.addToppingsCost = function(toppings) {
-//   if (toppings === "pepperoni") {
-//     this.totalPizzaCost += 3;
-//   } else if (toppings === "olives") {
-//     this.totalPizzaCost += 2;
-//   } else if (toppings === "cheese") {
-//     this.totalPizzaCost += 1;
-//   } else {
-//     alert("Please select at least one topping");
-//   }
-// }
-
 // User Interface Logic
 
 $(document).ready(function() {
   $("form#order-form").submit(function(event) {
     event.preventDefault();
     
-    let newPizza = new Pizza(inputtedSize, inputtedTopping);
-    const inputtedSize = $("input:radio[name=size]:checked").val();
-    newPizza.addSize(inputSize);
-
-  $("input:checkbox[name=topping]:checked").each(function() {
-    let inputtedTopping = $(this).val();
-    newPizza.addToppingsCost(inputTopping);
-  });
+    let veggies = [];
+    $("input[name=veggies]:checked").each(function () {
+      veggies.push($(this).val());
+    });
     
   });
 });
