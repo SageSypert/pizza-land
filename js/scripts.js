220f744593fb765f6c79261901e1766541084327
Sage Sypert <sage.sypert@gmail.com>
2021-01-29 13:13:03 -0800

    add if else statements to the add topping pizza prototype

--- js/scripts.js
+++ js/scripts.js
@@ -4,37 +4,43 @@
 // Business Logic
 
 function Pizza() {
-  this.size = size,
-  this.toppings = [],
-  this.totalPizzaCost = 0
+  this.size = size;
+  this.toppings = [];
+  this.totalPizzaCost = 0;
 }
 
 Pizza.prototype.addSize = function(size) {
   this.size = size;
 }
 
-Pizza.prototype.addTopping = function(topping) {
-  this.toppings.push(topping);
-}
-
-Pizza.prototype.calculatePrice = function() {
+Pizza.prototype.calculateSizePrice = function() {
   if (this.size === "small") {
-    this.price += 12;
+    this.totalPizzaCost += 12;
   } else if (this.size === "medium") {
-    this.price += 18;
+    this.totalPizzaCost += 18;
   } else if (this.size === "large") {
-    this.price += 24;
+    this.totalPizzaCost += 24;
+  } else {
+    alert("Please select size")
   }
-  for (let index=0; i < this.toppings.length; i++) {
-    const threeDollarToppings = ["pepperoni", "salami", "bacon"];
-    const twoDollarToppings = ["anchovies", "feta", "canadian bacon", "basil"]
-    const oneDollarToppings = ["olives", "pineapple", "mushrooms", "garlic"]
-  }
-
+}
   
-
-
-
+Pizza.prototype.addTopping = function() {
+  if (this.toppings === "olives" || "pineapple" || "mushrooms" || "garlic" || "cheese") {
+    this.totalPizzaCost += 1;
+  } else if (this.toppings === "anchovies" || "feta" || "canadian bacon" || "basil") {
+    this.totalPizzaCost += 2;
+  } else if (this.toppings === "pepperoni" || "salami" || "bacon") {
+    this.totalPizzaCost += 3;
+  } else {
+    alert("Please choose at least one topping")
+  }
+}
+  
+  // for (let index=0; i < this.toppings.length; i++) {
+  //   const threeDollarToppings = ["pepperoni", "salami", "bacon"];
+  //   const twoDollarToppings = ["anchovies", "feta", "canadian bacon", "basil"]
+  //   const oneDollarToppings = ["olives", "pineapple", "mushrooms", "garlic"]
 
 // User Interface Logic
 
