511ecc985ccb4c2518f053f1a16cc61f43c9cc21
Sage Sypert <sage.sypert@gmail.com>
2021-01-29 11:54:08 -0800

    add size, topping, and calculate price prototype

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,40 @@
 function Pizza() {
   this.size = size;
   this.toppings = [];
-  this.toppingsCost = 0;
-  this.sizeCost = 0;
+  // this.toppingsCost = 0;
+  // this.sizeCost = 0;
   this.totalPizzaCost = 0;
 }
 
-// Determine cost of user's pizza based on size
-
 Pizza.prototype.addSize = function(size) {
   this.size = size;
-  if (this.size === "small") {
-    this.sizePrice = 10;
-  } else if (this.size === "medium") {
-    this.sizePrice === 15;
-  } else {
-    this.sizePrice = 20;
-  }
-  this.calculateTotal();
 }
 
+Pizza.prototype.addTopping = function(topping) {
+  this.toppings.push(topping);
+}
+
+Pizza.prototype.calculatePrice = function() {
+  if (this.size === "small") {
+    this.price += 12;
+  } else if (this.size === "medium") {
+    this.price += 18;
+  } else (this.size === "large") {
+    this.price += 24;
+  }
+
+
+
+// Pizza.prototype.addSize = function(size) {
+//   this.size = size;
+//   if (this.size === "small") {
+//     this.totalPizzaCost = 10;
+//   } else if (this.size === "medium") {
+//     this.totalPizzaCost === 15;
+//   } else {
+//     this.totalPizzaCost = 20;
+//   }
+// }
 
 
 
