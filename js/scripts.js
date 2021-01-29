d53c1d2df2c4796ecc61507fc43a1c9202a9dc00
Sage Sypert <sage.sypert@gmail.com>
2021-01-29 15:26:00 -0800

    re-do loops for meat, sauce, and veggies else if statements to start from scratch

--- js/scripts.js
+++ js/scripts.js
@@ -6,38 +6,57 @@
 function Pizza(size, toppings, totalPizzaCost) {
   this.size = size;
   this.meat = meat;
-  this.toppings = toppings;
+  this.veggies = veggies;
+  this.sauce = sauce;
   this.totalPizzaCost = 0;
 }
 
-// Pizza.prototype.addSize = function(size) {
-//   this.size = size;
-// }
-
-Pizza.prototype.calculateSizePrice = function(size) {
+Pizza.prototype.calculatePrice = function() {
   if (this.size === "small") {
     this.totalPizzaCost += 12;
   } else if (this.size === "medium") {
     this.totalPizzaCost += 18;
   } else if (this.size === "large") {
     this.totalPizzaCost += 24;
-  } else {
-    alert("Please select a size");
+  } 
+
+  this.veggies.forEach(val => {
+    if (["olives", "onion", "spinach"].indexOf(val) != -1) {
+      this.totalPizzaCost += 1
+    } else if (["mushrooms", "garlic", "kalamata-olives"].indexOf(val) != -1) {
+      this.totalPizzaCost += 2
+    }
+  });
+
+  if (this.meat === "salami" || this.meat === "pepperoni" || this.meat === "chicken") {
+    this.totalPizzaCost += 2.50
+  } else if (this.meat === "canadian" || this.meat === "sausage") {
+    this.totalPizzaCost += 3
   }
+
+  if (this.sauce === "marinara" || this.sauce === "garlic") {
+    this.totalPizzaCost += 1.50
+  } else if (this.sauce === "buffalo" || this.sauce === "pesto") {
+    this.totalPizzaCost += 2
+  }
+
+  return this.totalPizzaCost
+
 }
+
   
 
-Pizza.prototype.addToppingsCost = function(toppings) {
-  if (toppings === "pepperoni") {
-    this.totalPizzaCost += 3;
-  } else if (toppings === "olives") {
-    this.totalPizzaCost += 2;
-  } else if (toppings === "cheese") {
-    this.totalPizzaCost += 1;
-  } else {
-    alert("Please select at least one topping");
-  }
-}
+// Pizza.prototype.addToppingsCost = function(toppings) {
+//   if (toppings === "pepperoni") {
+//     this.totalPizzaCost += 3;
+//   } else if (toppings === "olives") {
+//     this.totalPizzaCost += 2;
+//   } else if (toppings === "cheese") {
+//     this.totalPizzaCost += 1;
+//   } else {
+//     alert("Please select at least one topping");
+//   }
+// }
 
 // User Interface Logic
 
