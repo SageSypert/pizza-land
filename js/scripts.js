c132e1ec4859f580d53d816b7b285aec31f5cfc3
Sage Sypert <sage.sypert@gmail.com>
2021-01-29 08:46:07 -0800

    add more outline notes to scripts. research JS objects and constructors

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// Outline
+
+//Create pizza parlor site with the below functionality:
+//User can choose one or more individual toppings
+//User can choose a pizza size
+//Return the final pizza cost
+
+//Create a pizza object constructor with properties for toppings and size
+//Create a prototype method for the cost of a pizza depending on selections chosen
+
+
+// Business Logic
+
+
+
+
+
+
+
+
+
+
+
+
+
+// User Interface Logic
