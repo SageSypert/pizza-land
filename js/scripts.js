21f2e5b767e8e85cd98d0ff1d33585149aae17bd
Sage Sypert <sage.sypert@gmail.com>
2021-01-29 14:15:06 -0800

    add functionality and research - submit button not producing

--- js/scripts.js
+++ js/scripts.js
@@ -13,24 +13,44 @@ function Pizza(size, toppings, totalPizzaCost) {
 //   this.size = size;
 // }
 
-Pizza.prototype.calculatePrice = function() {
+Pizza.prototype.calculateSizePrice = function(size) {
   if (this.size === "small") {
     this.totalPizzaCost += 12;
   } else if (this.size === "medium") {
     this.totalPizzaCost += 18;
-  } else {
+  } else if (this.size === "large") {
     this.totalPizzaCost += 24;
-  }
-  if (this.toppings <= 4) {
-    this.totalPizzaCost += 4;
-  } else if (this.toppings >= 5 && this.toppings <= 8) {
-    this.totalPizzaCost += 8;
-  } else if (this.toppings >= 9 && this.toppings <= 12) {
-    this.totalPizzaCost += 12;
   } else {
-    alert("Please choose at least one option!");
+    alert("Please select a size");
   }
-} 
+}
+  
+
+Pizza.prototype.addToppingsCost = function(toppings) {
+  if (toppings === "pepperoni") {
+    this.totalPizzaCost += 3;
+  } else if (toppings === "olives") {
+    this.totalPizzaCost += 2;
+  } else if (toppings === "cheese") {
+    this.totalPizzaCost += 1;
+  } else {
+    alert("Please select at least one topping");
+  }
+}
+
+
+
+
+//   if (this.toppings <= 4) {
+//     this.totalPizzaCost += 4;
+//   } else if (this.toppings >= 5 && this.toppings <= 8) {
+//     this.totalPizzaCost += 8;
+//   } else if (this.toppings >= 9 && this.toppings <= 12) {
+//     this.totalPizzaCost += 12;
+//   } else {
+//     alert("Please choose at least one option!");
+//   }
+// } 
   
   // for (let index=0; i < this.toppings.length; i++) {
   //   const threeDollarToppings = ["pepperoni", "salami", "bacon"];
@@ -38,10 +58,19 @@ Pizza.prototype.calculatePrice = function() {
   //   const oneDollarToppings = ["olives", "pineapple", "mushrooms", "garlic"]
 
 // User Interface Logic
+let newPizza = new pizzaOrder();
 
 $(document).ready(function() {
   $("form#order-form").submit(function(event) {
     event.preventDefault();
-    let toppings = parseInt($("#toppings").val());
-  }
-}
+    let userPizza = new Pizza();
+    let inputSize = $("input:radio[name=size]:checked").val();
+    userPizza.addSize(inputSize);
+    $("input:checkbox[name=topping]:checked").each(function() {
+      let inputTopping = $(this).val();
+      userPizza.addToppingsCost(inputTopping);
+    });
+
+
+  });
+});
